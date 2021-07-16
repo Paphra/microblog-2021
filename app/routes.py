63fa1bbbd881491f8a68ca29a978848e562813ff
Epaphradito Lugayavu <paphra.me@gmail.com>
2021-07-17 00:58:39 +0300

    Ended Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
-	
+	user = {
+		'username': 'Epaphradito',
+	}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Suzan'},
+			'body': 'The Marvel Studio is so Cool!'
+		}
+	]
+
+	return render_template('index.html', title="Home", user=user, posts=posts)
