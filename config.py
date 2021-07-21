45d6e6b9e0f6bd00f2ce1f2acb1ef5b5b18c1229
Epaphradito Lugayavu <paphra.me@gmail.com>
2021-07-22 01:08:47 +0300

    Pagination  - Keeping the real

--- config.py
+++ config.py
@@ -15,3 +15,7 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['paphra.me@gmail.com']
+
+    # Pagination
+    POSTS_PER_PAGE = 3
+    
