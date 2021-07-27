f342b5a612a925c3e829b82a323b9283f8618e0d
Epaphradito Lugayavu <paphra.me@gmail.com>
2021-07-28 00:59:13 +0300

    heroku deployment changes

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'something big'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -25,4 +26,6 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
 
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_SDOUT')
     
